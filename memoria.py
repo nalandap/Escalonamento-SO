5783ff868c3dfcaeff25415c8962f54cf86e33e8
Laura Ferreira de Almeida <laura.ferreira@ufba.br>
2025-01-31 20:43:50 -0300

    comentando todas as funções de paginação

--- memoria.py
+++ memoria.py
@@ -1,44 +1,28 @@
-class RAM:
-    def __init__(self, capacidade):
-        self.capacidade = capacidade  # Capacidade em número de páginas
-        self.paginas = []  # Lista de páginas na RAM
+#class RAM:
+    #def __init__(self, capacidade):
+        
+    #def adicionar_pagina(self, pagina):
+  
+        
 
-    def adicionar_pagina(self, pagina):
-        """Adiciona uma página à RAM, se houver espaço."""
-        if len(self.paginas) < self.capacidade:
-            self.paginas.append(pagina)
-            return True
-        return False  # RAM cheia
+    #def remover_pagina(self, pagina):
+  
+      
 
-    def remover_pagina(self, pagina):
-        """Remove uma página da RAM."""
-        if pagina in self.paginas:
-            self.paginas.remove(pagina)
-            return True
-        return False  # Página não encontrada
-
-    def esta_cheia(self):
-        """Verifica se a RAM está cheia."""
-        return len(self.paginas) >= self.capacidade
-
-    def __str__(self):
-        return f"RAM: {[pagina['id'] for pagina in self.paginas]}"
+    #def esta_cheia(self):
 
 
-class Disco:
-    def __init__(self):
-        self.paginas = []  # Lista de páginas no Disco
+    #def __str__(self):
 
-    def adicionar_pagina(self, pagina):
-        """Adiciona uma página ao Disco."""
-        self.paginas.append(pagina)
 
-    def remover_pagina(self, pagina):
-        """Remove uma página do Disco."""
-        if pagina in self.paginas:
-            self.paginas.remove(pagina)
-            return True
-        return False  # Página não encontrada
+#class Disco:
+    #def __init__(self):
+      # self.paginas = []  # Lista de páginas no Disco
 
-    def __str__(self):
-        return f"Disco: {[pagina['id'] for pagina in self.paginas]}"
+    #def adicionar_pagina(self, pagina):
+
+
+    #def remover_pagina(self, pagina):
+     
+    #def __str__(self):
+        #return f"Disco: {[pagina['id'] for pagina in self.paginas]}"
--- substituicao.py
+++ substituicao.py
@@ -1,24 +1,7 @@
-from memoria import RAM, Disco
-from substituicao import fifo_substituicao, lru_substituicao
+#from memoria import RAM, Disco
 
-# Inicializa a RAM (capacidade de 3 páginas) e o Disco
-ram = RAM(capacidade=3)
-disco = Disco()
+#def fifo_substituicao(ram, disco, nova_pagina):
+   
 
-# Páginas de exemplo
-pagina1 = {'id': 1, 'ultimo_acesso': 0}
-pagina2 = {'id': 2, 'ultimo_acesso': 1}
-pagina3 = {'id': 3, 'ultimo_acesso': 2}
-pagina4 = {'id': 4, 'ultimo_acesso': 3}
-
-# Adiciona páginas à RAM
-print(fifo_substituicao(ram, disco, pagina1))  # Adiciona página 1 à RAM
-print(fifo_substituicao(ram, disco, pagina2))  # Adiciona página 2 à RAM
-print(fifo_substituicao(ram, disco, pagina3))  # Adiciona página 3 à RAM
-
-# RAM está cheia, substitui a página mais antiga (FIFO)
-print(fifo_substituicao(ram, disco, pagina4))  # Substitui página 1 por página 4
-
-# Exibe o estado da RAM e do Disco
-print(ram)  # RAM: [2, 3, 4]
-print(disco)  # Disco: [1]
+#def lru_substituicao(ram, disco, nova_pagina):
+    #return
