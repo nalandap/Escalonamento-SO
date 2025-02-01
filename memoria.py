f05c1fcebcb9df88d9785e38031e2d93484e8b05
Laura Ferreira de Almeida <laura.ferreira@ufba.br>
2025-02-01 12:05:10 -0300

    mudanças na funcao substituir pag

--- memoria.py
+++ memoria.py
@@ -1,29 +1,43 @@
 import time
+
 class RAM:
     def __init__(self, capacidade):
-        self.capacidade = capacidade
-        self.paginas = []  
-        self.tempo_acesso = {} 
+        self.capacidade = capacidade  # Capacidade em MB (1 página = 4 MB)
+        self.paginas = []  # Lista de páginas na RAM
+        self.tempo_acesso = {}  # Dicionário para rastrear o tempo de acesso das páginas
 
     def adicionar_pagina(self, pagina):
-        if len(self.paginas) * 4 < self.capacidade: 
+        if len(self.paginas) * 4 < self.capacidade:  # Verifica se há espaço na RAM
             self.paginas.append(pagina)
-            self.tempo_acesso[pagina['id']] = time.time()  
+            self.tempo_acesso[pagina['id']] = time.time()  # Atualiza o tempo de acesso
             return True
         return False
 
     def remover_pagina(self, pagina):
         if pagina in self.paginas:
             self.paginas.remove(pagina)
-            del self.tempo_acesso[pagina['id']]
+            del self.tempo_acesso[pagina['id']]  # Remove o tempo de acesso
             return True
         return False
+
+    def substituir_pagina(self, nova_pagina):
+        if self.esta_cheia():
+            # Encontra a página mais antiga (FIFO)
+            pagina_mais_antiga = min(self.tempo_acesso, key=self.tempo_acesso.get)
+            for pagina in self.paginas:
+                if pagina['id'] == pagina_mais_antiga:
+                    pagina_removida = pagina
+                    self.remover_pagina(pagina_removida)
+                    self.adicionar_pagina(nova_pagina)
+                    return pagina_removida  # Retorna a página removida para ser movida para o disco
+        return None
 
     def esta_cheia(self):
         return len(self.paginas) * 4 >= self.capacidade
 
     def __str__(self):
         return f"RAM: {[pagina['id'] for pagina in self.paginas]}"
+
 
 class Disco:
     def __init__(self):
@@ -40,3 +54,5 @@ class Disco:
 
     def __str__(self):
         return f"Disco: {[pagina['id'] for pagina in self.paginas]}"
+
+
