4169230c52018eb4be54501869d620df382a083f
Nalanda <nalandaspita@gmail.com>
2025-02-02 21:17:10 -0300

    retornando

--- substituicao.py
+++ substituicao.py
@@ -2,27 +2,26 @@ from memoria import RAM, Disco
 import time
 
 def substituir_pagina_fifo(ram, disco, nova_pagina):
-    pagina_removida = None
     if ram.esta_cheia():
-        pagina_removida = ram.paginas.pop(0)  # Remove a primeira página (FIFO)
-        disco.adicionar_pagina(pagina_removida)  # Move a página para o disco
-        del ram.tempo_acesso[pagina_removida['id']]  # Remove o tempo de acesso
-        
-    ram.adicionar_pagina(nova_pagina)  # Adiciona a nova página na RAM
-    return pagina_removida  # Retorna a página removida para que possa ser manipulada
+        pagina_removida = ram.paginas.pop(0)  
+        disco.adicionar_pagina(pagina_removida)
+        del ram.tempo_acesso[pagina_removida['id']] 
+    
+    ram.adicionar_pagina(nova_pagina)  
+
 
 
 def substituir_pagina_lru(ram, disco, nova_pagina):
-    pagina_removida = None
     if ram.esta_cheia():
-        # Encontra a página menos recentemente utilizada (LRU)
+        # Encontra a página menos recentemente utilizada
         pagina_lru = min(ram.tempo_acesso, key=ram.tempo_acesso.get)
         for pagina in ram.paginas:
             if pagina['id'] == pagina_lru:
                 pagina_removida = pagina
                 break
-        ram.remover_pagina(pagina_removida)  # Remove a página da RAM
-        disco.adicionar_pagina(pagina_removida)  # Move para o disco
+        ram.remover_pagina(pagina_removida)
+        disco.adicionar_pagina(pagina_removida)
+    ram.adicionar_pagina(nova_pagina)
+
+
     
-    ram.adicionar_pagina(nova_pagina)  # Adiciona a nova página na RAM
-    return pagina_removida  # Retorna a página removida para que possa ser manipulada
